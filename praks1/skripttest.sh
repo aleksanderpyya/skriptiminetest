a6805485c17e9cd4d46452da9e4ed80177be143c
Aleksander Püüa <aleksander.puua@voco.ee>
2024-10-23 12:48:29 +0300

    see on esimene skript

---
+++ praks1/skripttest.sh
@@ -0,0 +1,10 @@
+!#/bin/bash
+### see on testimiseks esimene skript
+echo "Oled kasutaja nimega: "
+whoami
+sleep1
+echo "ja sina asud kataloogis:"
+pwd
+sleep1
+echo "selle kataloogi sisu(list) on: "
+ls -l
